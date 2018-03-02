caf5f704492e47e0cd09d3e246efdf847d9ca278
Tim Reilly <tireilly@microsoft.com>
2018-03-02 14:23:12 -0800

    ui ready for testing

--- src/js/index.js
+++ src/js/index.js
@@ -122,8 +122,78 @@ class App extends React.Component {
         this.state.ContractInstance = MyContract.at("0x8f0483125fcb9aaaefa9209d8e9d7b9c8b9fb90f")
     }
 
-    voteNumber(number) {
-        console.log(number);
+    componentDidMount(){
+        this.updateState()
+        this.setupListeners()
+
+        setInterval(this.updateState.bind(this), 10e3); 
+    }
+
+    updateState() {
+        this.state.ContractInstance.minimumBet((err, result) => {
+            if(result != null){
+                this.setState({
+                    minimumBet: parseFloat(web3.fromWei(result, 'ether'))
+                })
+            }
+        })
+        this.state.ContractInstance.totalBet((err, result) => {
+            if(result != null){
+                this.setState({
+                    totalBet: parseFloat(web3.fromWei(result, 'ether'))
+                })
+            }
+        })
+        this.state.ContractInstance.numberOfBets((err, result) => {
+            if(result != null){
+                this.setState({
+                    numberOfbets: parseInt(result)
+                })
+            }
+        })
+        this.state.ContractInstance.maxAmountOfBets((err, result) => {
+            if(result != null){
+                this.setState({
+                    maxAmountOfBets: parseInt(result) 
+                })
+            }
+        })
+    }
+
+    // Listen for events and executes the voteNumber method
+
+    setupListeners(){
+        let liNodes = this.refs.numbers.querySelectorAll('li')
+        liNodes.forEach(number => {
+            number.addEventListener('click', event => {
+                event.target.className = 'number-selected'
+                this.voteNumber(parseInt(event.target.innerHTML), done => {
+                    // Remove the other number selected
+                    for (let i = 0; i < liNodes.length; i++){
+                        liNodes[i].className = ''
+                    }
+                })
+            })
+        })
+    }
+
+    voteNumber(number, cb){
+        let bet = this.refs['ether-bet'].value; 
+
+        if(!bet) bet = 0.1; 
+
+        if(parseFloat(bet) < this.state.minimumBet){
+            alert('you must bet more than the minimum') 
+            cb()
+        } else {
+            this.state.ContractInstance.bet(number, {
+                gas: 300000,
+                from: web3.eth.accounts[0], 
+                value: web3.toWei(bet, 'ether')
+            }, (err, result) => {
+                cb()
+            })
+        }
     }
 
     render() {
@@ -134,26 +204,43 @@ class App extends React.Component {
                     <h4>Time:</h4> &nbsp;
                     <span ref="timer"> {this.state.timer}</span>
                 </div>
-
+                
+                <div className="block">
+                    <b>Number of bets: </b> &nbsp; 
+                    <span>{this.state.numberOfBets}</span> 
+                </div> 
+    
                 <div className="block">
                     <h4>Last winner: </h4> &nbsp;
                 <span ref="last-winner">{this.state.lastWinner}</span>
                 </div>
 
+                <div className="block">
+                    <b>Total ether bet: </b> &nbsp; 
+                    <span>{this.state.totalBet}</span> 
+
+                </div>
+
                 <hr />
 
-                <h2>Vot for the next number</h2>
-                <ul>
-                    <li onClick={() => { this.voteNumber(1) }}>1</li>
-                    <li onClick={() => { this.voteNumber(2) }}>2</li>
-                    <li onClick={() => { this.voteNumber(3) }}>3</li>
-                    <li onClick={() => { this.voteNumber(4) }}>4</li>
-                    <li onClick={() => { this.voteNumber(5) }}>5</li>
-                    <li onClick={() => { this.voteNumber(6) }}>6</li>
-                    <li onClick={() => { this.voteNumber(7) }}>7</li>
-                    <li onClick={() => { this.voteNumber(8) }}>8</li>
-                    <li onClick={() => { this.voteNumber(9) }}>9</li>
-                    <li onClick={() => { this.voteNumber(10) }}>10</li>
+                <h2>Vote for the next number</h2>
+
+                <label>
+                    <b>How much Ether do you want to bet? <input className="bet-input" ref="ether-bet" type="number" placeholder={this.state.minimumBet}/>
+                    </b> ether 
+                    <br/>
+                </label>
+                <ul ref="number">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                    <li>8</li>
+                    <li>9</li>
+                    <li>10</li>
                 </ul>
 
 
