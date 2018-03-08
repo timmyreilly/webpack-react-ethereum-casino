bd26bb18b1f340142d418181858771d951f2cc71
Tim Reilly <tireilly@microsoft.com>
2018-03-08 15:07:20 -0800

    ui almost working

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,92 +1,91 @@
-pragma solidity ^0.4.11; 
+pragma solidity ^0.4.11;
 
 contract Casino {
-    address owner;
-    uint minimumBet = 100 finney; 
-    uint totalBet; 
-    uint numberOfBets; 
-    uint maxAmountOfBets = 100; 
-    address[] players; 
+   address owner;
+   uint public minimumBet = 100 finney; // Equal to 0.1 ether
+   uint public totalBet;
+   uint public numberOfBets;
+   uint public maxAmountOfBets = 100;
+   uint public numberWinner;
+   address[] public players;
 
-    struct Player {
-        uint amountBet; 
-        uint numberSelected; 
-    }
+   struct Player {
+      uint amountBet;
+      uint numberSelected;
+   }
 
-    mapping(address => Player) playerInfo; 
-    
-    function Casino(uint _minimumBet, uint _maxAmountOfBets){
-        owner = msg.sender; 
-        if(_minimumBet != 0) minimumBet = _minimumBet; 
-        if(_maxAmountOfBets != 0) maxAmountOfBets = _maxAmountOfBets; 
-    }
+   mapping(address => Player) playerInfo;
 
-    // Fallback function in case someone sends ether to the contract so it doesn't get lost 
-    function() payable {}
+   function Casino(uint _minimumBet, uint _maxAmountOfBets){
+      owner = msg.sender;
+      if(_minimumBet != 0) minimumBet = _minimumBet;
+      if(_maxAmountOfBets != 0) maxAmountOfBets = _maxAmountOfBets;
+   }
 
-    function kill() {
-        if(msg.sender == owner){
-            selfdestruct(owner);  
-        }
-    }
+   // Fallback function in case someone sends ether to the contract so it doesn't get lost
+   function() payable {}
 
-    function checkPlayerExists(address player) returns(bool) {
-        for(uint i = 0; i < players.length; i++){
-            if(players[i] == player) return true; 
-        }
-        return false; 
-    }
+   function kill(){
+      if(msg.sender == owner)
+      selfdestruct(owner);
+   }
 
-    function bet(uint number) payable{
-        assert(checkPlayerExists(msg.sender) == false); 
-        assert(number >= 1 && number <= 10);
-        assert(msg.value >= minimumBet); 
+   function checkPlayerExists(address player) returns(bool){
+      for(uint i = 0; i < players.length; i++){
+         if(players[i] == player) return true;
+      }
+      return false;
+   }
 
-        playerInfo[msg.sender].amountBet = msg.value; 
-        playerInfo[msg.sender].numberSelected = number; 
-        numberOfBets += 1; 
-        players.push(msg.sender);
-        totalBet += msg.value; 
+   // To bet for a number between 1 and 10 both inclusive
+   // To bet for a number between 1 and 10 both inclusive
+   function bet(uint number) payable{
+      assert(checkPlayerExists(msg.sender) == false);
+      assert(number >= 1 && number <= 10);
+      assert(msg.value >= minimumBet);
 
-        if(numberOfBets >= maxAmountOfBets) generateNumberWinner(); 
-    }
+      playerInfo[msg.sender].amountBet = msg.value;
+      playerInfo[msg.sender].numberSelected = number;
+      numberOfBets += 1;
+      players.push(msg.sender);
+      totalBet += msg.value;
 
+      if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
+   }
 
-    function generateNumberWinner(){
-        uint numberGenerated = block.number % 10 + 1; // This isn't secure 
-        distributePrizes(numberGenerated); 
-    }
+   // Generates a number between 1 and 10
+   function generateNumberWinner(){
+      numberWinner = block.number % 10 + 1; // This isn't secure
+      distributePrizes();
+   }
 
-    // Send the corresponding ether to each winner depending on the total bets 
+   // Sends the corresponding ether to each winner depending on the total bets
+   function distributePrizes(){
+      address[100] memory winners; // We have to create a temporary in memory array with fixed size
+      uint count = 0; // This is the count for the array of winners
 
-    function distributePrizes(uint numberWinner){
-        address[100] memory winners; // We have to create a temporary in memory array with fixed size
+      for(uint i = 0; i < players.length; i++){
+         address playerAddress = players[i];
+         if(playerInfo[playerAddress].numberSelected == numberWinner){
+            winners[count] = playerAddress;
+            count++;
+         }
+         delete playerInfo[playerAddress]; // Delete all the players
+      }
 
-        uint count = 0; // This the count for the array of winners 
+      uint winnerEtherAmount = totalBet / winners.length; // How much each winner gets
 
-        for(uint i = 0; i < players.length; i++){
-            address playerAddress = players[i]; 
-            if(playerInfo[playerAddress].numberSelected == numberWinner){
-                winners[count] = playerAddress; 
-                count++;
-            }
-            delete playerInfo[playerAddress]; // delete all the players 
-        }
+      for(uint j = 0; j < count; j++){
+         if(winners[j] != address(0)) // Check that the address in this fixed array is not empty
+         winners[j].transfer(winnerEtherAmount);
+      }
 
-        uint winnerEtherAmount = totalBet / winners.length;  // How much each winner gets 
+      resetData();
+   }
 
-        for(uint j = 0; j < count; j++){
-            if(winners[j] != address(0)){ // Check that the address in this fixed array is not empty 
-                winners[j].transfer(winnerEtherAmount); 
-            }
-        }
-
-        resetData();
-    }
-
-    function resetData() {
-        players.length = 0; // Delete all the players array 
-        totalBet = 0; 
-        numberOfBets = 0; 
-    }
+   function resetData(){
+      players.length = 0; // Delete all the players array
+      totalBet = 0;
+      numberOfBets = 0;
+   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,6 @@ var Casino = artifacts.require("./Casino.sol");
 
 module.exports = function(deployer) {
   // this worked: deployer.deploy(Casino, 1, 3);
-  web3.toWei(0.1, 'ether'), 100, {gas: 3000000} // This is better? 
+  // web3.toWei(0.1, 'ether'), 100, {gas: 3000000} // This is better?
+  deployer.deploy(Casino, 1, 3);  
 };
